a1880dc596eb670fb9cfeb721f8a5eb8663891a5
Halima KOUNDI <hkoundi@gmail.com>
2016-05-24 17:16:48 +0100

    step towards de duplication of Franc / Dollar

--- MultiCurrencyMoney/MultiCurrencyMoneyTests/MultiCurrencyMoneyTest.cs
+++ MultiCurrencyMoney/MultiCurrencyMoneyTests/MultiCurrencyMoneyTest.cs
@@ -8,32 +8,33 @@ namespace MultiCurrencyMoneyTests
         [Test]
         public void TestMultiplication()
         {
-            var five = new Dollar(5);
+            var five = Money.Dollar(5);
 
             var product = five.Times(2);
 
-            Assert.That(product, Is.EqualTo(new Dollar(10)));
+            Assert.That(product, Is.EqualTo(Money.Dollar(10)));
 
             product = five.Times(3);
 
-            Assert.That(product, Is.EqualTo(new Dollar(15)));
+            Assert.That(product, Is.EqualTo(Money.Dollar(15)));
         }
 
         [Test]
         public void TestFrancMultiplication()
         {
-            var fiveFranc   = new Franc(5);
+            var fiveFranc   = Money.Franc(5);
 
-            Assert.That(new Franc(10), Is.EqualTo(fiveFranc.Times(2)));
+            Assert.That(Money.Franc(10), Is.EqualTo(fiveFranc.Times(2)));
         }
 
         [Test]
         public void TestEquality()
         {
-            Assert.That(new Dollar(5).Equals(new Dollar(5)), Is.EqualTo(true));
-            Assert.That(new Dollar(5).Equals(new Dollar(6)), Is.EqualTo(false));
-            Assert.That(new Franc(5).Equals(new Franc(5)), Is.EqualTo(true));
-            Assert.That(new Franc(5).Equals(new Franc(6)), Is.EqualTo(false));
+            Assert.That(Money.Dollar(5).Equals(Money.Dollar(5)), Is.True);
+            Assert.That(Money.Dollar(5).Equals(Money.Dollar(6)), Is.False);
+            Assert.That(Money.Franc(5).Equals(Money.Franc(5)), Is.True);
+            Assert.That(Money.Franc(5).Equals(Money.Franc(6)), Is.False);
+            Assert.That(Money.Franc(5).Equals(Money.Dollar(5)), Is.False);
         }
     }
 }
