3c84c073fea8bdea4be1e54c0172cdb2ce114aec
Halima KOUNDI <hkoundi@gmail.com>
2016-05-31 16:53:48 +0100

    abstracting the Times method in the Expression interface

---
+++ MultiCurrencyMoney/MultiCurrencyMoneyTests/Pair.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiCurrencyMoneyTests
+{
+    class Pair
+    {
+        private string From;
+        private string To;
+
+        public Pair(string from, string to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var pair = (Pair) obj;
+            return From.Equals(pair.From) && To.Equals(pair.To);
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+}
